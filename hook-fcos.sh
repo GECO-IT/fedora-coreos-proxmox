d107e4508733b74b163c6108fa0e576ef8fb4b42
Cyril DUCHENOY <c.duchenoy@geco-it.fr>
2020-11-27 01:27:41 +0100

    Initial release
    Signed-off-by: Cyril DUCHENOY <c.duchenoy@geco-it.fr>

---
+++ hook-fcos.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+#set -e
+ 
+vmid="$1"
+phase="$2"
+
+# global vars
+COREOS_TMPLT=/opt/fcos-tmplt.yaml
+COREOS_FILES_PATH=/etc/pve/geco-pve/coreos
+YQ="/usr/local/bin/yq read --exitStatus --printMode v --stripComments --"
+
+# ==================================================================================================================================================================
+# functions()
+#
+setup_fcoreosct()
+{
+        local CT_VER=0.7.0
+        local ARCH=x86_64
+        local OS=unknown-linux-gnu # Linux
+        local DOWNLOAD_URL=https://github.com/coreos/fcct/releases/download
+ 
+        [[ -x /usr/local/bin/fcos-ct ]]&& [[ "x$(/usr/local/bin/fcos-ct --version | awk '{print $NF}')" == "x${CT_VER}" ]]&& return 0
+        echo "Setup Fedora CoreOS config transpiler..."
+        rm -f /usr/local/bin/fcos-ct
+        wget --quiet --show-progress ${DOWNLOAD_URL}/v${CT_VER}/fcct-${ARCH}-${OS} -O /usr/local/bin/fcos-ct
+        chmod 755 /usr/local/bin/fcos-ct
+}
+setup_fcoreosct
+
+setup_yq()
+{
+        local VER=3.4.1
+
+        [[ -x /usr/bin/wget ]]&& download_command="wget --quiet --show-progress --output-document"  || download_command="curl --location --output"
+        [[ -x /usr/local/bin/yq ]]&& [[ "x$(/usr/local/bin/yq --version | awk '{print $NF}')" == "x${VER}" ]]&& return 0
+        echo "Setup yaml parser tools yq..."
+        rm -f /usr/local/bin/yq
+        ${download_command} /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/${VER}/yq_linux_amd64
+        chmod 755 /usr/local/bin/yq
+}
+setup_yq
+
+# ==================================================================================================================================================================
+# main()
+#
+if [[ "${phase}" == "pre-start" ]]
+then
+	instance_id="$(qm cloudinit dump ${vmid} meta | ${YQ} - 'instance-id')"
+
+	# same cloudinit config ?
+	[[ -e ${COREOS_FILES_PATH}/${vmid}.id ]] && [[ "x${instance_id}" != "x$(cat ${COREOS_FILES_PATH}/${vmid}.id)" ]]&& {
+		rm -f ${COREOS_FILES_PATH}/${vmid}.ign # cloudinit config change
+	}
+	[[ -e ${COREOS_FILES_PATH}/${vmid}.ign ]]&& exit 0 # already done
+
+	mkdir -p ${COREOS_FILES_PATH} || exit 1
+		
+	# check config
+	cipasswd="$(qm cloudinit dump ${vmid} user | ${YQ} - 'password' 2> /dev/null)" || true # can be empty
+	[[ "x${cipasswd}" != "x" ]]&& VALIDCONFIG=true
+	${VALIDCONFIG:-false} || [[ "x$(qm cloudinit dump ${vmid} user | ${YQ} - 'ssh_authorized_keys[*]')" == "x" ]]|| VALIDCONFIG=true
+	${VALIDCONFIG:-false} || {
+		echo "Fedora CoreOS: you must set passwd or ssh-key before start VM${vmid}"
+		exit 1
+	}
+
+	echo -n "Fedora CoreOS: Generate yaml users block... "
+	echo -e "# This file is managed by Geco-iT hook-script. Do not edit.\n" > ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo -e "variant: fcos\nversion: 1.1.0" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo -e "# user\npasswd:\n  users:" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	ciuser="$(qm cloudinit dump ${vmid} user 2> /dev/null | grep ^user: | awk '{print $NF}')"
+	echo "    - name: \"${ciuser:-admin}\"" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo "      gecos: \"Geco-iT CoreOS Administrator\"" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo "      password_hash: '${cipasswd}'" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo '      groups: [ "sudo", "docker", "adm", "wheel", "systemd-journal" ]' >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo '      ssh_authorized_keys:' >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	qm cloudinit dump ${vmid} user | ${YQ} - 'ssh_authorized_keys[*]' | sed -e 's/^/        - "/' -e 's/$/"/' >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo "[done]"
+
+	echo -n "Fedora CoreOS: Generate yaml hostname block... "
+	hostname="$(qm cloudinit dump ${vmid} user | ${YQ} - 'hostname' 2> /dev/null)"
+	echo -e "# network\nstorage:\n  files:" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo "    - path: /etc/hostname" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo "      mode: 0644" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo "      overwrite: true" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo "      contents:" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo "        inline: |" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	echo -e "          ${hostname,,}\n" >> ${COREOS_FILES_PATH}/${vmid}.yaml 
+	echo "[done]"
+	
+	echo -n "Fedora CoreOS: Generate yaml network block... "
+	netcards="$(qm cloudinit dump ${vmid} network | ${YQ} - 'config[*].name' 2> /dev/null | wc -l)"
+	nameservers="$(qm cloudinit dump ${vmid} network | ${YQ} - "config[${netcards}].address[*]" | paste -s -d ";" -)"
+	searchdomain="$(qm cloudinit dump ${vmid} network | ${YQ} - "config[${netcards}].search[*]" | paste -s -d ";" -)"
+	for (( i=O; i<${netcards}; i++ ))
+	do
+		ipv4="" netmask="" gw="" macaddr="" # reset on each run
+		ipv4="$(qm cloudinit dump ${vmid} network | ${YQ} - config[${i}].subnets[0].address 2> /dev/null)" || continue # dhcp
+		netmask="$(qm cloudinit dump ${vmid} network | ${YQ} - config[${i}].subnets[0].netmask 2> /dev/null)"
+		gw="$(qm cloudinit dump ${vmid} network | ${YQ} - config[${i}].subnets[0].gateway 2> /dev/null)" || true # can be empty
+		macaddr="$(qm cloudinit dump ${vmid} network | ${YQ} - config[${i}].mac_address 2> /dev/null)"
+		# ipv6: TODO
+
+		echo "    - path: /etc/NetworkManager/system-connections/net${i}.nmconnection" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "      mode: 0600" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "      overwrite: true" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "      contents:" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "        inline: |" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "          [connection]" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "          type=ethernet" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "          id=net${i}" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "          #interface-name=eth${i}\n" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo -e "\n          [ethernet]" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "          mac-address=${macaddr}" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo -e "\n          [ipv4]" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "          method=manual" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "          addresses=${ipv4}/${netmask}" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "          gateway=${gw}" >> ${COREOS_FILES_PATH}/${vmid}.yaml 
+		echo "          dns=${nameservers}" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo -e "          dns-search=${searchdomain}\n" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+	done
+	echo "[done]"
+
+	[[ -e "${COREOS_TMPLT}" ]]&& {
+		echo -n "Fedora CoreOS: Generate other block based on template... "
+		cat "${COREOS_TMPLT}" >> ${COREOS_FILES_PATH}/${vmid}.yaml
+		echo "[done]"
+	}
+
+	echo -n "Fedora CoreOS: Generate ignition config... "
+	/usr/local/bin/fcos-ct 	--pretty --strict \
+				--output ${COREOS_FILES_PATH}/${vmid}.ign \
+				${COREOS_FILES_PATH}/${vmid}.yaml 2> /dev/null
+	[[ $? -eq 0 ]] || {
+		echo "[failed]"
+		exit 1
+	}
+	echo "[done]"
+
+	# save cloudinit instanceid
+	echo "${instance_id}" > ${COREOS_FILES_PATH}/${vmid}.id
+
+	# check vm config (no args on first boot)
+	qm config ${vmid} --current | grep -q ^args || {
+		echo -n "Set args com.coreos/config on VM${vmid}... "
+		rm -f /var/lock/qemu-server/lock-${vmid}.conf
+		pvesh set /nodes/$(hostname)/qemu/${vmid}/config --args "-fw_cfg name=opt/com.coreos/config,file=${COREOS_FILES_PATH}/${vmid}.ign" 2> /dev/null || {
+			echo "[failed]"
+			exit 1
+		}
+		touch /var/lock/qemu-server/lock-${vmid}.conf
+
+		# hack for reload new ignition file
+		echo -e "\nWARNING: New generated Fedora CoreOS ignition settings, we must restart vm..."
+		qm stop ${vmid} && sleep 2 && qm start ${vmid}&
+		exit 1
+	}
+fi
+
+exit 0
---
+++ vmsetup.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+#set -x # debug mode
+set -e
+
+# =============================================================================================
+# global vars
+
+# force english messages
+export LANG=C
+export LC_ALL=C
+
+# template vm vars
+TEMPLATE_VMID="900"
+TEMPLATE_VMSTORAGE="local"
+SNIPPET_STORAGE="local"
+VMDISK_OPTIONS=",discard=on"
+
+TEMPLATE_IGNITION="fcos-base-tmplt.yaml"
+
+# fcos version
+STREAMS=stable
+VERSION=32.20201018.3.0
+PLATEFORM=qemu
+BASEURL=https://builds.coreos.fedoraproject.org
+
+# =============================================================================================
+# main()
+
+# pve storage exist ?
+echo -n "Check if vm storage ${TEMPLATE_VMSTORAGE} exist... "
+pvesh get /storage/${TEMPLATE_VMSTORAGE} --noborder --noheader &> /dev/null || {
+        echo -e "[failed]"
+        exit 1
+}
+echo "[ok]"
+
+# pve storage snippet ok ?
+echo -n "Check if snippet storage ${SNIPPET_STORAGE} exist... "
+pvesh get /storage/${SNIPPET_STORAGE} --noborder --noheader &> /dev/null || {
+        echo -e "[failed]"
+        exit 1
+}
+echo "[ok]"
+
+# pve storage snippet enable
+pvesh get /storage/${SNIPPET_STORAGE} --noborder --noheader | grep -q snippets || {
+	echo "You musr activate content snippet on storage: ${SNIPPET_STORAGE}"
+	exit 1
+}
+
+# copy files
+echo "Copy hook-script and ignition config to snippet storage..."
+snippet_storage="$(pvesh get /storage/${SNIPPET_STORAGE} --noborder --noheader | grep ^path | awk '{print $NF}')"
+cp -av ${TEMPLATE_IGNITION} hook-fcos.sh ${snippet_storage}/snippets
+sed -e "/^COREOS_TMPLT/ c\COREOS_TMPLT=${snippet_storage}/snippets/${TEMPLATE_IGNITION}" -i ${snippet_storage}/snippets/hook-fcos.sh
+chmod 755 ${snippet_storage}/snippets/hook-fcos.sh
+
+# storage type ? (https://pve.proxmox.com/wiki/Storage)
+echo -n "Get storage \"${TEMPLATE_VMSTORAGE}\" type... "
+case "$(pvesh get /storage/${TEMPLATE_VMSTORAGE} --noborder --noheader | grep ^type | awk '{print $2}')" in
+        dir|nfs|cifs|glusterfs|cephfs) TEMPLATE_VMSTORAGE_type="file"; echo "[file]"; ;;
+        lvm|lvmthin|iscsi|iscsidirect|rbd|zfs|zfspool) TEMPLATE_VMSTORAGE_type="block"; echo "[block]" ;;
+        *)
+                echo "[unknown]"
+                exit 1
+        ;;
+esac
+
+# download fcos vdisk
+[[ ! -e fedora-coreos-${VERSION}-${PLATEFORM}.x86_64.qcow2 ]]&& {
+    echo "Download fedora coreos..."
+    wget -q --show-progress \
+        ${BASEURL}/prod/streams/${STREAMS}/builds/${VERSION}/x86_64/fedora-coreos-${VERSION}-${PLATEFORM}.x86_64.qcow2.xz
+    xz -dv fedora-coreos-${VERSION}-${PLATEFORM}.x86_64.qcow2.xz
+}
+
+# create a new VM
+echo "Create fedora coreos vm ${VMID}"
+qm create ${TEMPLATE_VMID} --name fcos-tmplt
+qm set ${TEMPLATE_VMID} --memory 4096 \
+			--cpu host \
+			--cores 4 \
+			--agent enabled=1 \
+			--autostart \
+			--onboot 1 \
+			--ostype l26 \
+			--tablet 0 \
+			--boot c --bootdisk scsi0
+
+template_vmcreated=$(date +%Y-%m-%d)
+qm set ${TEMPLATE_VMID} --description "Fedora CoreOS - Geco-iT Template
+
+ - Version             : ${VERSION}
+ - Cloud-init          : true
+
+Creation date : ${template_vmcreated}
+"
+
+qm set ${TEMPLATE_VMID} --net0 virtio,bridge=vmbr0
+#qm set ${TEMPLATE_VMID} --net1 virtio,bridge=vmbr1
+
+echo -e "\nCreate Cloud-init vmdisk..."
+qm set ${TEMPLATE_VMID} --ide2 ${TEMPLATE_VMSTORAGE}:cloudinit
+
+# import fedora disk
+if [[ "x${TEMPLATE_VMSTORAGE_type}" = "xfile" ]]
+then
+	vmdisk_name="${TEMPLATE_VMID}/vm-${TEMPLATE_VMID}-disk-0.qcow2"
+	vmdisk_format="--format qcow2"
+else
+	vmdisk_name="vm-${TEMPLATE_VMID}-disk-0"
+        vmdisk_format=""
+fi
+qm importdisk ${TEMPLATE_VMID} fedora-coreos-${VERSION}-${PLATEFORM}.x86_64.qcow2 ${TEMPLATE_VMSTORAGE} ${vmdisk_format}
+qm set ${TEMPLATE_VMID} --scsihw virtio-scsi-pci --scsi0 ${TEMPLATE_VMSTORAGE}:${vmdisk_name}${VMDISK_OPTIONS}
+
+# set hook-script
+qm set ${TEMPLATE_VMID} -hookscript ${SNIPPET_STORAGE}:snippets/hook-fcos.sh
+
+
+# convert vm template
+echo -n "Convert VM ${TEMPLATE_VMID} in proxmox vm template... "
+qm template ${TEMPLATE_VMID} &> /dev/null || true
+echo "[done]"
